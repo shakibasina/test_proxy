1696159e1b0ab0aa2dc864690628d77125432b25
sina shakiba <123159273+shakibasina@users.noreply.github.com>
2026-05-24 20:56:56 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import axios from "axios";
+
 export default {
-  async fetch(request) {
-    return new Response("OK")
+  async fetch() {
+    const res = await axios.get("https://httpbin.org/get");
+
+    return new Response(JSON.stringify(res.data));
   }
 }
